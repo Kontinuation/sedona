7872aed1583e75545d76969c04122a5f35c3ad37
Jia Yu <jiayu198910@gmail.com>
2023-02-20 19:50:23 -0800

    Fix the version detector in Sedona Python

--- python/sedona/core/jvm/config.py
+++ python/sedona/core/jvm/config.py
@@ -143,7 +143,7 @@ class SedonaMeta:
     def get_version(cls, spark_jars: str) -> Optional[str]:
         # Find Spark version, Scala version and Sedona version.
         versions = findall(
-            r"sedona-(?:python-adapter|spark-shaded)-([^,\n]+)_([^,\n]+)-([^,\n]+)",
+            r"sedona-(?:python-adapter|spark-shaded)-([^,\n]{3})_([^,\n]{4})-([^,\n]{5})",
             spark_jars,
         )
         try:
