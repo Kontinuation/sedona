2556f60946c43a6810c24cb663b10c9c5c9cdb9c
Nilesh Gajwani <nileshgajwani@gmail.com>
2023-06-22 21:33:32 -0700

    [SEDONA-213] Add ST_BoundingDiagonal (#863)

--- sql/common/src/main/scala/org/apache/spark/sql/sedona_sql/expressions/Functions.scala
+++ sql/common/src/main/scala/org/apache/spark/sql/sedona_sql/expressions/Functions.scala
@@ -1056,3 +1056,9 @@ case class ST_Translate(inputExpressions: Seq[Expression])
   }
 }
 
+case class ST_BoundingDiagonal(inputExpressions: Seq[Expression])
+  extends InferredUnaryExpression(Functions.boundingDiagonal) with FoldableExpression {
+  protected def withNewChildrenInternal(newChildren: IndexedSeq[Expression]) = {
+    copy(inputExpressions = newChildren)
+  }
+}
