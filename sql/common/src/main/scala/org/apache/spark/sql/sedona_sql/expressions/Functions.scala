81705e2083952044ac259f30e3646fc0a60b5539
Nilesh Gajwani <nileshgajwani@gmail.com>
2023-06-26 06:44:58 -0700

    [SEDONA-299] Add ST_FrechetDistance (#869)

--- sql/common/src/main/scala/org/apache/spark/sql/sedona_sql/expressions/Functions.scala
+++ sql/common/src/main/scala/org/apache/spark/sql/sedona_sql/expressions/Functions.scala
@@ -1040,6 +1040,13 @@ case class ST_Translate(inputExpressions: Seq[Expression])
   }
 }
 
+case class ST_FrechetDistance(inputExpressions: Seq[Expression])
+  extends InferredExpression(Functions.frechetDistance _) with FoldableExpression {
+  protected def withNewChildrenInternal(newChildren: IndexedSeq[Expression]) = {
+    copy(inputExpressions = newChildren)
+  }
+}
+
 case class ST_Affine(inputExpressions: Seq[Expression])
   extends InferredExpression(InferrableFunction.allowSixRightNull(Functions.affine _)) with FoldableExpression {
   protected def withNewChildrenInternal(newChildren: IndexedSeq[Expression]) = {
