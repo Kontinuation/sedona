1949f531e6ec89317734f3b35cee523c5e544230
Junhao Liu <992364620@qq.com>
2023-06-28 01:37:26 +0800

    [SEDONA-238] Implement OGC GeometryType (#873)

--- sql/common/src/main/scala/org/apache/spark/sql/sedona_sql/expressions/Functions.scala
+++ sql/common/src/main/scala/org/apache/spark/sql/sedona_sql/expressions/Functions.scala
@@ -1060,6 +1060,7 @@ case class ST_Dimension(inputExpressions: Seq[Expression])
     copy(inputExpressions = newChildren)
   }
 }
+
 case class ST_BoundingDiagonal(inputExpressions: Seq[Expression])
   extends InferredExpression(Functions.boundingDiagonal _) {
   protected def withNewChildrenInternal(newChildren: IndexedSeq[Expression]) = {
@@ -1069,6 +1070,13 @@ case class ST_BoundingDiagonal(inputExpressions: Seq[Expression])
 
 case class ST_HausdorffDistance(inputExpressions: Seq[Expression])
   extends InferredExpression(inferrableFunction3(Functions.hausdorffDistance)) {
+  protected def withNewChildrenInternal(newChildren: IndexedSeq[Expression]) = {
+    copy(inputExpressions = newChildren)
+  }
+}
+
+case class GeometryType(inputExpressions: Seq[Expression])
+  extends InferredExpression(Functions.geometryTypeWithMeasured _) with FoldableExpression {
   protected def withNewChildrenInternal(newChildren: IndexedSeq[Expression]) = {
     copy(inputExpressions = newChildren)
   }
