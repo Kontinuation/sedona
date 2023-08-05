69aab260d34dcd914046edad5e518f785f65a731
Junhao Liu <992364620@qq.com>
2023-08-06 04:32:43 +0800

    [SEDONA-342] Implement ST_Polygon (#944)

--- sql/common/src/main/scala/org/apache/sedona/sql/UDF/Catalog.scala
+++ sql/common/src/main/scala/org/apache/sedona/sql/UDF/Catalog.scala
@@ -128,6 +128,7 @@ object Catalog {
     function[ST_SubDivideExplode](),
     function[ST_SubDivide](),
     function[ST_MakeLine](),
+    function[ST_Polygon](),
     function[ST_MakePolygon](null),
     function[ST_GeoHash](),
     function[ST_GeomFromGeoHash](null),
