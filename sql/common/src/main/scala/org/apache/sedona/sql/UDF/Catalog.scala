75d1d67abaccfb637806a944cee09dd03549a229
Furqaanahmed Khan <46216254+furqaankhan@users.noreply.github.com>
2023-08-12 03:23:25 -0400

    [SEDONA-354] Add RS_SkewX and RS_SkewY (#960)

--- sql/common/src/main/scala/org/apache/sedona/sql/UDF/Catalog.scala
+++ sql/common/src/main/scala/org/apache/sedona/sql/UDF/Catalog.scala
@@ -220,6 +220,8 @@ object Catalog {
     function[RS_ScaleX](),
     function[RS_ScaleY](),
     function[RS_BandPath](),
+    function[RS_SkewX](),
+    function[RS_SkewY](),
     function[RS_PixelAsPoint](),
     function[RS_ConvexHull](),
     function[RS_RasterToWorldCoordX](),
--- sql/common/src/main/scala/org/apache/spark/sql/sedona_sql/expressions/raster/RasterAccessors.scala
+++ sql/common/src/main/scala/org/apache/spark/sql/sedona_sql/expressions/raster/RasterAccessors.scala
@@ -84,6 +84,18 @@ case class RS_BandPath(inputExpressions: Seq[Expression]) extends InferredExpres
   }
 }
 
+case class RS_SkewX(inputExpressions: Seq[Expression]) extends InferredExpression(RasterAccessors.getSkewX _) {
+  protected def withNewChildrenInternal(newChildren: IndexedSeq[Expression]): Expression = {
+    copy(inputExpressions = newChildren)
+  }
+}
+
+case class RS_SkewY(inputExpressions: Seq[Expression]) extends InferredExpression(RasterAccessors.getSkewY _) {
+  protected def withNewChildrenInternal(newChildren: IndexedSeq[Expression]): Expression = {
+    copy(inputExpressions = newChildren)
+  }
+}
+
 case class RS_RasterToWorldCoordX(inputExpressions: Seq[Expression]) extends InferredExpression(RasterAccessors.getWorldCoordX _) {
   protected def withNewChildrenInternal(newChildren: IndexedSeq[Expression]) = {
     copy(inputExpressions = newChildren)
