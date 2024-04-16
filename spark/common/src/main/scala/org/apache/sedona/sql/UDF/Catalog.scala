0916ea428b962cfb4f14036d57e4ba51383a7e05
Furqaan Khan <46216254+furqaankhan@users.noreply.github.com>
2024-04-16 02:57:46 -0400

    [TASK-78] Add ST_ForceCollection (#164)
    * feat: Add ST_ForceCollection
    
    * fix: snowflake tests
    
    * fix: snowflake tests
    
    * chore: remove print statement
    
    * chore: add GeometryCollection test
    
    * docs: add detailed explanation

--- spark/common/src/main/scala/org/apache/sedona/sql/UDF/Catalog.scala
+++ spark/common/src/main/scala/org/apache/sedona/sql/UDF/Catalog.scala
@@ -193,6 +193,7 @@ object Catalog {
     function[ST_LengthSpheroid](),
     function[ST_NumPoints](),
     function[ST_Force3D](0.0),
+    function[ST_ForceCollection](),
     function[ST_NRings](),
     function[ST_Translate](0.0),
     function[ST_TriangulatePolygon](),
--- spark/common/src/main/scala/org/apache/spark/sql/sedona_sql/expressions/Functions.scala
+++ spark/common/src/main/scala/org/apache/spark/sql/sedona_sql/expressions/Functions.scala
@@ -1200,6 +1200,15 @@ case class ST_Force3D(inputExpressions: Seq[Expression])
   }
 }
 
+case class ST_ForceCollection(inputExpressions: Seq[Expression])
+  extends InferredExpression(Functions.forceCollection _) {
+
+  protected def withNewChildrenInternal(newChildren: IndexedSeq[Expression]) = {
+    copy(inputExpressions = newChildren)
+  }
+}
+
+
 case class ST_ForcePolygonCW(inputExpressions: Seq[Expression])
   extends InferredExpression(Functions.forcePolygonCW _) {
   protected def withNewChildrenInternal(newChildren: IndexedSeq[Expression]) = {
