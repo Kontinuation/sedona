a3d97da66defc13bf02698373695338417e843fe
Furqaan Khan <46216254+furqaankhan@users.noreply.github.com>
2024-04-26 15:06:38 -0400

    fix: bug of ST_LongestLine (#173)
    * temp commit
    
    * Revert "temp commit"
    
    This reverts commit 8cd8ecc6b3ae533379b779ba08c23b12df47e2fc.
    
    * feat: Add ST_LongestLine
    
    * fix: bug that would cause null pointer exception

--- common/src/main/java/org/apache/sedona/common/Functions.java
+++ common/src/main/java/org/apache/sedona/common/Functions.java
@@ -1059,7 +1059,7 @@ public class Functions {
     }
 
     public static Geometry longestLine(Geometry geom1, Geometry geom2) {
-        double maxLength = 0;
+        double maxLength = - Double.MAX_VALUE;
         Coordinate longestStart = null;
         Coordinate longestEnd = null;
 
