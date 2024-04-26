b12a7d68961649caabe983f1c64074843f6da724
Furqaan Khan <46216254+furqaankhan@users.noreply.github.com>
2024-04-25 22:42:37 -0400

    [TASK-32] Add ST_LongestLine (#169)
    * temp commit
    
    * Revert "temp commit"
    
    This reverts commit 8cd8ecc6b3ae533379b779ba08c23b12df47e2fc.
    
    * feat: Add ST_LongestLine

--- spark/common/src/main/scala/org/apache/sedona/sql/UDF/Catalog.scala
+++ spark/common/src/main/scala/org/apache/sedona/sql/UDF/Catalog.scala
@@ -152,6 +152,7 @@ object Catalog {
     function[ST_LineSubstring](),
     function[ST_LineInterpolatePoint](),
     function[ST_LineLocatePoint](),
+    function[ST_LongestLine](),
     function[ST_SubDivideExplode](),
     function[ST_SubDivide](),
     function[ST_MakeLine](),
