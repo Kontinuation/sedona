b42d4e67648e9b0a3cca65dd111abe52a173d860
Furqaanahmed Khan <furqaankhan98@gmail.com>
2024-05-28 14:58:39 -0400

    feat: Add ST_LocateAlong

--- spark/common/src/main/scala/org/apache/sedona/sql/UDF/Catalog.scala
+++ spark/common/src/main/scala/org/apache/sedona/sql/UDF/Catalog.scala
@@ -158,6 +158,7 @@ object Catalog {
     function[ST_LineSubstring](),
     function[ST_LineInterpolatePoint](),
     function[ST_LineLocatePoint](),
+    function[ST_LocateAlong](),
     function[ST_LongestLine](),
     function[ST_SubDivideExplode](),
     function[ST_SubDivide](),
