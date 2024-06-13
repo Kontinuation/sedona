f1e38828778ca361d10ad94ced2fbe3bac1fafed
Furqaan Khan <46216254+furqaankhan@users.noreply.github.com>
2024-06-13 00:52:24 -0400

    [SEDONA-608] Fix ST_IsPolygonCW, ST_IsPolygonCCW, ST_ForcePolygonCW and ST_ForcePolygonCCW (#1476)
    * fix: interior ring bug
    
    * fix: spotless error

--- common/src/main/java/org/apache/sedona/common/Functions.java
+++ common/src/main/java/org/apache/sedona/common/Functions.java
@@ -1036,6 +1036,10 @@ public class Functions {
     LinearRing exteriorRing = geom.getExteriorRing();
     boolean isExteriorRingCW = !Orientation.isCCW(exteriorRing.getCoordinateSequence());
 
+    if (geom.getNumInteriorRing() == 0) {
+      return isExteriorRingCW;
+    }
+
     boolean isInteriorRingCW = Orientation.isCCW(geom.getInteriorRingN(0).getCoordinateSequence());
     for (int i = 1; i < geom.getNumInteriorRing(); i++) {
       isInteriorRingCW =
@@ -1144,6 +1148,10 @@ public class Functions {
   private static boolean checkIfPolygonCCW(Polygon geom) {
     LinearRing exteriorRing = geom.getExteriorRing();
     boolean isExteriorRingCCW = Orientation.isCCW(exteriorRing.getCoordinateSequence());
+
+    if (geom.getNumInteriorRing() == 0) {
+      return isExteriorRingCCW;
+    }
 
     boolean isInteriorRingCCW =
         !Orientation.isCCW(geom.getInteriorRingN(0).getCoordinateSequence());
