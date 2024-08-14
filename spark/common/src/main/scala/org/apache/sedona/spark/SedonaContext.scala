cc7f06ae359e9b939fb0300bbb165e2c57a01bf5
Feng Zhang <feng@wherobots.com>
2024-08-13 20:34:10 -0700

    [SEDONA-636] Cherry-Pick Support GEOMETRY in Spark SQL table creation DDL (#257)
    * [SEDONA-636] Support GEOMETRY in Spark SQL table creation DDL (#1543)
    
    * [SEDONA-636] Support geometry data type in create table DDL statement
    
    * add one more test
    
    * [SEDONA-636] Support GEOMETRY in Spark SQL table creation DDL
    
    * add one more unit test
    
    * temporarily remove spark 3.0.3 build
    
    * Support multiple versions of spark
    
    * clean up files
    
    * undo github workflow changes to java.yaml
    
    * add parser to spark-3.0
    
    * add sedona parser to spark-3.0 profile
    
    * fix python pipeline

--- spark/common/src/main/scala/org/apache/sedona/spark/SedonaContext.scala
+++ spark/common/src/main/scala/org/apache/sedona/spark/SedonaContext.scala
@@ -21,7 +21,7 @@ package org.apache.sedona.spark
 import org.apache.log4j.Logger
 import org.apache.sedona.common.utils.TelemetryCollector
 import org.apache.sedona.core.serde.SedonaKryoRegistrator
-import org.apache.sedona.sql.RasterRegistrator
+import org.apache.sedona.sql.{ParserRegistrator, RasterRegistrator}
 import org.apache.sedona.sql.UDF.UdfRegistrator
 import org.apache.sedona.sql.UDT.UdtRegistrator
 import org.apache.spark.SparkConf
@@ -78,6 +78,7 @@ object SedonaContext {
     UdtRegistrator.registerAll()
     UdfRegistrator.registerAll(sparkSession)
     ListenerRegistrator.registerAll(sparkSession)
+    ParserRegistrator.register(sparkSession)
     jsc = new JavaSparkContext(sparkSession.sparkContext)
     jconf = jsc.getConf
     try {
