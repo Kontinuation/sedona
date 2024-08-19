9751b2297d1ecafabf4a7f5003cfe6976a48ab86
Furqaan Khan <46216254+furqaankhan@users.noreply.github.com>
2024-08-19 13:53:59 -0400

    [SEDONA-645] Add ST_RotateX (#1554)
    * feat: Add ST_RotateX
    
    * chore: remove show statement
    
    * fix: snowflake test
    
    * docs: add new sedona versioning

--- spark/common/src/main/scala/org/apache/sedona/sql/UDF/Catalog.scala
+++ spark/common/src/main/scala/org/apache/sedona/sql/UDF/Catalog.scala
@@ -234,6 +234,7 @@ object Catalog {
     function[ST_AKNN](),
     function[ST_IsValidReason](),
     function[ST_Rotate](),
+    function[ST_RotateX](),
     // Expression for rasters
     function[RS_NormalizedDifference](),
     function[RS_Mean](),
