1167b1ba8710e7f0f72a8bee45a35bbfd433cf96
Kristin Cowalcijk <kontinuation@apache.org>
2024-11-16 02:53:55 +0800

    [SEDONA-674] Update the sedona-raster python module to support GDAL 3.10 (#1682)

--- python/sedona/raster/sedona_raster.py
+++ python/sedona/raster/sedona_raster.py
@@ -17,6 +17,7 @@
 
 from abc import ABC, abstractmethod
 from typing import Dict, List, Optional, Tuple
+import json
 from xml.etree.ElementTree import Element, SubElement, tostring
 
 import numpy as np
@@ -33,21 +34,50 @@ from .gdal_conf import get_rasterio_aws_session
 from .meta import AffineTransform, OutDbMeta, PixelAnchor, SampleDimension
 
 
+GDAL_VERSION = rasterio.env.GDALVersion.runtime()
+
+
+def _has_env_with_gdal_mem_enabled():
+    if rasterio.env.hasenv():
+        if GDAL_VERSION.at_least(rasterio.env.GDALVersion(3, 10)):
+            # For GDAL >= 3.10, GDAL_MEM_ENABLE_OPEN must be enabled to load
+            # MEM:: dataset. Please refer to
+            # https://gdal.org/en/latest/drivers/raster/mem.html for details.
+            options = rasterio.env.getenv()
+            return options.get("GDAL_MEM_ENABLE_OPEN") == "YES"
+        else:
+            return True
+    else:
+        return False
+
+
 def _rasterio_open(fp, driver=None):
     session = get_rasterio_aws_session(fp)
     if session:
         aws_no_sign_request = "YES" if session.unsigned else "NO"
-        with rasterio.Env(session=session, AWS_NO_SIGN_REQUEST=aws_no_sign_request):
+        with rasterio.Env(
+            session=session,
+            AWS_NO_SIGN_REQUEST=aws_no_sign_request,
+            GDAL_MEM_ENABLE_OPEN="YES",
+        ):
             return rasterio.open(fp, mode="r", driver=driver)
     else:
-        return rasterio.open(fp, mode="r", driver=driver)
+        if not _has_env_with_gdal_mem_enabled():
+            with rasterio.Env(GDAL_MEM_ENABLE_OPEN="YES"):
+                return rasterio.open(fp, mode="r", driver=driver)
+        else:
+            return rasterio.open(fp, mode="r", driver=driver)
 
 
 def _rasterio_open_memfile(path, memfile: MemoryFile, driver=None, band_indices=None):
     session = get_rasterio_aws_session(path)
     if session:
         aws_no_sign_request = "YES" if session.unsigned else "NO"
-        with rasterio.Env(session=session, AWS_NO_SIGN_REQUEST=aws_no_sign_request):
+        with rasterio.Env(
+            session=session,
+            AWS_NO_SIGN_REQUEST=aws_no_sign_request,
+            GDAL_MEM_ENABLE_OPEN="YES",
+        ):
             ds = memfile.open(driver=driver)
             arr = None
             # We need to perform a read to initialize the VSI file handles here
@@ -64,7 +94,11 @@ def _rasterio_open_memfile(path, memfile: MemoryFile, driver=None, band_indices=
                     ds.read(window=Window(0, 0, 1, 1))
             return ds, arr
     else:
-        return memfile.open(driver=driver), None
+        if not _has_env_with_gdal_mem_enabled():
+            with rasterio.Env(GDAL_MEM_ENABLE_OPEN="YES"):
+                return memfile.open(driver=driver), None
+        else:
+            return memfile.open(driver=driver), None
 
 
 def _normalize_path(src_path: str) -> str:
@@ -232,31 +266,33 @@ class InDbSedonaRaster(SedonaRaster):
             f"{self._affine_trans.ip_x}/{self._affine_trans.scale_x}/{self._affine_trans.skew_x}/"
             + f"{self._affine_trans.ip_y}/{self._affine_trans.skew_y}/{self._affine_trans.scale_y}"
         )
-        # FIXME: GDAL 3.6 shipped with rasterio does not support
-        # SPATIALREFERENCE parameter, so we have to workaround this issue in a
-        # hacky way. If newer versions of rasterio bundle GDAL 3.7 then this
-        # won't be a problem. See https://gdal.org/drivers/raster/mem.html
         desc = (
             f"MEM:::DATAPOINTER={data_pointer},PIXELS={self._width},LINES={self._height},BANDS={num_bands},"
             + f"DATATYPE={data_type},GEOTRANSFORM={geotransform}"
         )
 
-        # construct a VRT to wrap this MEM dataset, with SRS set up properly
-        vrt_xml = OutDbSedonaRaster.generate_vrt_xml(
-            desc,
-            data_type,
-            self._width,
-            self._height,
-            geotransform.replace("/", ","),
-            self._crs_wkt,
-            0,
-            0,
-            list(range(num_bands)),
-        )
-
-        # dataset = _rasterio_open(desc, driver="MEM")
-        self.rasterio_memfile = MemoryFile(vrt_xml, ext=".vrt")
-        dataset = self.rasterio_memfile.open(driver="VRT")
+        # If we are using GDAL >= 3.7, we can use the SPATIALREFERENCE
+        # parameter; otherwise we have to wrap the MEM dataset with an VRT to
+        # set up the SRS.
+        if GDAL_VERSION.at_least(rasterio.env.GDALVersion(3, 7)):
+            escaped_srs = json.dumps(self._crs_wkt.replace("\n", ""))
+            desc += f",SPATIALREFERENCE={escaped_srs}"
+            dataset = _rasterio_open(desc, driver="MEM")
+        else:
+            # construct a VRT to wrap this MEM dataset, with SRS set up properly
+            vrt_xml = OutDbSedonaRaster.generate_vrt_xml(
+                desc,
+                data_type,
+                self._width,
+                self._height,
+                geotransform.replace("/", ","),
+                self._crs_wkt,
+                0,
+                0,
+                list(range(num_bands)),
+            )
+            self.rasterio_memfile = MemoryFile(vrt_xml, ext=".vrt")
+            dataset = _rasterio_open_memfile("", self.rasterio_memfile, driver="VRT")[0]
 
         # XXX: dataset does not copy the data held by data_array, so we set
         # data_array as a property of dataset to make sure that the lifetime of
--- python/sedona/sql/st_functions.py
+++ python/sedona/sql/st_functions.py
@@ -2420,6 +2420,7 @@ def ST_GetReverseGeocodingLayers() -> Column:
 
     return _call_st_function("ST_GetReverseGeocodingLayers", ())
 
+
 @validate_argument_types
 def ST_InterpolatePoint(geom1: ColumnOrName, geom2: ColumnOrName) -> Column:
     """Returns the interpolated Measure value at the point on the given linestring M that is closest to the given point.
